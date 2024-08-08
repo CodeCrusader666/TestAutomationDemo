8228d654de7e7f8ec797721890926c8ca3e9bff8
CodeCrusader666 <inch.by.inch.everythings.a.cinch@gmail.com>
2024-08-08 18:45:37 +0300

    Implementing test for registration form(in progress)

--- tests/ui/registration_form.spec.ts
+++ tests/ui/registration_form.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
+import {LoginPage} from '../page_objects/registration_form_page';
 
 test('Form should display an error if required fields are missing', async ({ page }) => {
+  const registrationFormPage = new RegistrationFormPage(page);
+  await registrationFormPage.open();
+  await registrationFormPage.setName('Ihor');
+  await registrationFormPage.setSecondName('Hoffman');
 });
 
 test('Form should successfully save date if optional fields are missing', async ({ page }) => {
