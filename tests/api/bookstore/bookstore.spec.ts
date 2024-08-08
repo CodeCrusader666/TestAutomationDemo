482f723e49e823fdd0baa64e38fb8cb5d49e46d9
CodeCrusader666 <inch.by.inch.everythings.a.cinch@gmail.com>
2024-08-08 18:35:40 +0300

    Fixed usage of 'request' object

--- tests/api/bookstore/bookstore.spec.ts
+++ tests/api/bookstore/bookstore.spec.ts
@@ -1,22 +1,22 @@
-import { request, test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 const BOOKS_ENDPOINT = '/BookStore/v1/Books';
 const BOOK_ENDPOINT = '/BookStore/v1/Book';
 
-test('GET all books', async ({ page }) => {
+test('GET all books', async ({ request }) => {
   const allBooksResponse = await request.get(BOOKS_ENDPOINT);
   expect(allBooksResponse.status()).toBe(200);
   // ToDo: verify response
 });
 
-test('GET book by ISBN', async ({ page }) => {
+test('GET book by ISBN', async ({ request }) => {
   const ISBN = '9781449331818';
   const bookResponse = await request.get(`${BOOK_ENDPOINT}/${ISBN}`);
   expect(bookResponse.status()).toBe(200);
   // ToDo: verify response
 });
 
-test('GET book using wrong ISBN', async ({ page }) => {
+test('GET book using wrong ISBN', async ({ request }) => {
   const WRONG_ISBN = '9781442222222';
   const bookResponse = await request.get(`${BOOK_ENDPOINT}/${WRONG_ISBN}`);
   expect(bookResponse.status()).toBe(400);
