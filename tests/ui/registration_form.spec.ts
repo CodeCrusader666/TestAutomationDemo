74f0da75f76734b82b13f60cb3425054566f8fd2
CodeCrusader666 <inch.by.inch.everythings.a.cinch@gmail.com>
2024-08-08 19:06:16 +0300

    Registration form: Added before each for common logic

--- tests/ui/registration_form.spec.ts
+++ tests/ui/registration_form.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { RegistrationFormPage } from '../../page_objects/registration_form_page';
 
-test('Form should display an error if required fields are missing', async ({ page }) => {
-  const registrationFormPage = new RegistrationFormPage(page);
+let registrationFormPage: RegistrationFormPage;
+
+test.beforeEach(async ({ page }) => {
+  registrationFormPage = new RegistrationFormPage(page);
   await registrationFormPage.open();
+});
+
+test('Form should display an error if required fields are missing', async ({ page }) => {
   await registrationFormPage.setName('Ihor');
   await registrationFormPage.setSecondName('Hoffman');
+  await registrationFormPage.submitForm();
 });
 
 test('Form should successfully save date if optional fields are missing', async ({ page }) => {
