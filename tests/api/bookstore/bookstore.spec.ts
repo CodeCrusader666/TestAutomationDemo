ee6a9a11e74786f9b0c24c5d9636448536831849
CodeCrusader666 <inch.by.inch.everythings.a.cinch@gmail.com>
2024-08-08 17:44:13 +0300

    Skeleton for bookstore api tests

--- tests/api/bookstore/bookstore.spec.ts
+++ tests/api/bookstore/bookstore.spec.ts
@@ -1,1 +1,10 @@
+import { test, expect } from '@playwright/test';
 
+test('GET all books', async ({ page }) => {
+});
+
+test('GET book by ISBN', async ({ page }) => {
+});
+
+test('GET book using wrong ISBN', async ({ page }) => {
+});
